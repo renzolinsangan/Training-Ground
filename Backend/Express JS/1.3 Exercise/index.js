3bdbc93956ae564908e49ba435bc8c519ff627fd
renzolinsangan29 <renzolinsangan11@gmail.com>
2025-11-18 15:54:18 +0800

    Step by Step Process for HTTP Requests and running server using HTTP and Ports.

--- Backend/Express JS/1.3 Exercise/index.js
+++ Backend/Express JS/1.3 Exercise/index.js
@@ -6,8 +6,12 @@ app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>");
 });
 
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Page</h1>");
+});
+
 app.get("/about", (req, res) => {
-    res.send("<h1>About Me</h1>")
+    res.send("<h1>About Page</h1>");
 });
 
 app.listen(3000, () => {
