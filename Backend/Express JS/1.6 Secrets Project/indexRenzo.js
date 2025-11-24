e10cf123d889db24de17c95c89bbfa4e97c88247
renzolinsangan29 <renzolinsangan11@gmail.com>
2025-11-24 17:25:37 +0800

    Created Renzo Version and Solution Version

--- Backend/Express JS/1.6 Secrets Project/indexRenzo.js
+++ Backend/Express JS/1.6 Secrets Project/indexRenzo.js
@@ -7,30 +7,20 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-var userIsAuthorised = false;
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-function passwordCheck(req, res, next) {
-  const password = req.body["password"];
-  if (password === "ILoveProgramming") {
-    userIsAuthorised = true;
-  }
-  next();
-}
-app.use(passwordCheck);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-  if (userIsAuthorised) {
-    res.sendFile(__dirname + "/public/secret.html");
-  } else {
-    res.sendFile(__dirname + "/public/index.html");
-    //Alternatively res.redirect("/");
-  }
+    var inputPassword = req.body['password'];
+
+    if(inputPassword === 'ILoveProgramming') {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
 });
 
 app.listen(port, () => {
